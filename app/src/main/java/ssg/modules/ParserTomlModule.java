6e516e8ddbef47eb499199f8903d4d01f6a25727
ARNOUX robin <julia.arnoux@etu.univ-paris-diderot.fr>
2022-04-12 22:32:58 +0200

    Issue42 | Connection buildpage / parserTOML + corrections

--- app/src/main/java/ssg/modules/ParserTomlModule.java
+++ app/src/main/java/ssg/modules/ParserTomlModule.java
@@ -2,10 +2,14 @@ package ssg.modules;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
+import ssg.buildsite.BuildSite;
+import ssg.buildsite.BuildSiteImplementation;
 import ssg.filereader.FileReader;
 import ssg.filereader.FileReaderImplementation;
 import ssg.filewriter.FileWriter;
 import ssg.filewriter.FileWriterImplementation;
+import ssg.parsertoml.ParserToml;
+import ssg.parsertoml.ParserTomlImplementation;
 
 /**
  * Module class for ParserToml.
@@ -18,6 +22,7 @@ public class ParserTomlModule extends AbstractModule {
      */
     @Override
     protected void configure() {
+        bind(ParserToml.class).to(ParserTomlImplementation.class);
         bind(FileReader.class)
             .annotatedWith(Names.named("FileReader"))
             .to(FileReaderImplementation.class);
